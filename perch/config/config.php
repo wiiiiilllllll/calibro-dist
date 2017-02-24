ef8ea160d034cdeb66f736a15a7f26bbcd5dcf6d
Will Wallace <will@willwallace.name>
2017-02-24 17:48:31 +0000

    LIVE PERCH CONFIG - EDITED ON SERVER

--- perch/config/config.php
+++ perch/config/config.php
@@ -1,12 +1,12 @@
 <?php
     define('PERCH_LICENSE_KEY', 'P21609-ZDW650-GYZ757-HCN154-PHD019');
 
-    define("PERCH_DB_USERNAME", 'perch');
-    define("PERCH_DB_PASSWORD", 'fKv%6JR!K%NDDXdm8X7e4Kan2Q^WzePQ');
-    define("PERCH_DB_SERVER", "localhost");
-    define("PERCH_DB_DATABASE", "calibro");
+    define("PERCH_DB_USERNAME", 'willwall_calibro');
+    define("PERCH_DB_PASSWORD", 'calibroconsultants');
+    define("PERCH_DB_SERVER", "10.169.0.133");
+    define("PERCH_DB_DATABASE", "willwall_calibro");
     define("PERCH_DB_PREFIX", "perch2_");
-    
+
     define('PERCH_TZ', 'Europe/London');
 
     define('PERCH_EMAIL_FROM', 'will@willwallace.name');
@@ -16,7 +16,6 @@
     define('PERCH_PATH', str_replace(DIRECTORY_SEPARATOR.'config', '', __DIR__));
     define('PERCH_CORE', PERCH_PATH.DIRECTORY_SEPARATOR.'core');
 
-    define('PERCH_RESFILEPATH', PERCH_PATH . DIRECTORY_SEPARATOR . 'resources');
-    define('PERCH_RESPATH', PERCH_LOGINPATH . '/resources');
-    
+    define('PERCH_RESFILEPATH', PERCH_PATH . DIRECTORY_SEPARATOR . 'resources');    define('PERCH_RESPATH', PERCH_LOGINPATH . '/resources');
+
     define('PERCH_HTML5', true);
