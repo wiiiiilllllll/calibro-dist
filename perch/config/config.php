e23e6975220076291417b46d8d0c08a322426cd0
Will Wallace <wwallace@siteset.co.uk>
2017-03-06 18:47:10 +0000

    Bring updated perch config from production branch

--- perch/config/config.php
+++ perch/config/config.php
@@ -1,10 +1,30 @@
 <?php
     define('PERCH_LICENSE_KEY', 'P21609-ZDW650-GYZ757-HCN154-PHD019');
 
-    define("PERCH_DB_USERNAME", 'willwall_calibro');
-    define("PERCH_DB_PASSWORD", 'calibroconsultants');
-    define("PERCH_DB_SERVER", "10.169.0.133");
-    define("PERCH_DB_DATABASE", "willwall_calibro");
+    $http_host = getenv('HTTP_HOST');
+    switch($http_host) {
+        case('192.168.33.10') :
+            define("PERCH_DB_USERNAME", 'perch');
+            define("PERCH_DB_PASSWORD", 'fKv%6JR!K%NDDXdm8X7e4Kan2Q^WzePQ');
+            define("PERCH_DB_SERVER", "localhost");
+            define("PERCH_DB_DATABASE", "calibro");
+            break;
+
+        case('willwallace.xyz') :
+            define("PERCH_DB_USERNAME", 'willwall_calibro');
+            define("PERCH_DB_PASSWORD", 'calibroconsultants');
+            define("PERCH_DB_SERVER", "10.169.0.133");
+            define("PERCH_DB_DATABASE", "willwall_calibro");
+            break;
+
+        default :
+            define("PERCH_DB_USERNAME", 'c4libroconsultan');
+            define("PERCH_DB_PASSWORD", 'juz4rR@wEJ&PgJ7VnjwqfUBx');
+            define("PERCH_DB_SERVER", "localhost");
+            define("PERCH_DB_DATABASE", "c4libroc_perch");
+            break;
+    }
+
     define("PERCH_DB_PREFIX", "perch2_");
 
     define('PERCH_TZ', 'Europe/London');
