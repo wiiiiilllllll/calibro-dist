7c7aa036580f36bbcaa49951937255aeef3687cc
Will Wallace <wwallace@siteset.co.uk>
2017-03-06 16:41:41 +0000

    Move comment (see previous) to correct file

--- perch/templates/layouts/global.head.php
+++ perch/templates/layouts/global.head.php
@@ -4,7 +4,6 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
 <head>
-    <!-- OK -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title><?php perch_pages_title(); ?> - Calibro Transport, Highway &amp; Infrastructure Consultants</title>
