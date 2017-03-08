0979936e3a4501663cad92628cf377f57c56e272
Will Wallace <wwallace@siteset.co.uk>
2017-03-08 19:26:06 +0000

    Better perch config; all external links now open in new tab

--- js/calibro.js
+++ js/calibro.js
@@ -1,3 +1,14 @@
+// Courtesy of CSS Tricks:
+// https://css-tricks.com/snippets/jquery/open-external-links-in-new-window/
+
+$(function(){	
+	$('a').each(function() {
+	   var a = new RegExp('/' + window.location.host + '/');
+	   if(!a.test(this.href)) {
+	       $(this).attr('target', '_blank');
+	   }
+	});
+})
 var blocks = {
 	
 	init: function() {
