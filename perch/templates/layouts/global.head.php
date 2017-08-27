1b522340346dc70d9ccc7001d05ec5f2b30e0369
Will Wallace <wwallace@siteset.co.uk>
2017-08-27 17:31:24 +0100

    Fix markup bugs w/ meta description & services abstracts; deleted old commented-out scss

--- perch/templates/layouts/global.head.php
+++ perch/templates/layouts/global.head.php
@@ -7,13 +7,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title><?php (perch_layout_has("title")?perch_layout_var("title"):perch_pages_title()); ?> - Calibro Transport, Highway &amp; Infrastructure Consultants</title>
-    <meta name="description" content='<?php perch_page_attributes("description"); ?>'>
+    <?php perch_page_attributes("description"); ?>
+    <link rel="canonical" href="<?php perch_page_url(); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
     <link rel="manifest" href="/manifest.json">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#a1bf35">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg">
     <meta name="theme-color" content="#afcb4a">
     <link rel="stylesheet" href="/css/calibro.min.css">
     <!--[if lt IE 9]><script src="{{'/js/html5shiv.js'}}"></script><![endif]-->
