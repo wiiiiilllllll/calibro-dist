63f11363925dfec1982176e6e35e7c8da7c91f17
Will Wallace <wwallace@siteset.co.uk>
2017-08-27 18:41:47 +0100

    Add twitter/facebook open graph tags

--- perch/templates/layouts/global.head.php
+++ perch/templates/layouts/global.head.php
@@ -6,10 +6,18 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title><?php (perch_layout_has("title")?perch_layout_var("title"):perch_pages_title()); ?> - Calibro Transport, Highway &amp; Infrastructure Consultants</title>
+    <title><?php (perch_layout_has("title")?perch_layout_var("title"):perch_pages_title()); ?> - Calibro Consultants</title>
     <?php perch_page_attributes("description"); ?>
-    <link rel="canonical" href="<?php perch_page_url(); ?>">
+    <link rel="canonical" href="<?php perch_page_url();?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@CalibroConsult">
+    <meta property="og:type" content="website">
+    <meta property="og:title" name="twitter:title" content="<?php (perch_layout_has("title")?perch_layout_var("title"):perch_pages_title());?>">
+    <meta property="og:url" name="twitter:domain" content="<?php perch_page_url();?>">
+    <meta property="og:image" name="twitter:image:src" content="/img/calibro-logo-transparent@2x.png">
+    <meta property="og:site_name" content="Calibro Consultants">
+    <meta property="og:description" name="twitter:description" content="<?php perch_page_attribute("description");?>">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
