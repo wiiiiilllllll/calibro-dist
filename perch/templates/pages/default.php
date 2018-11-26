2cd7e88b6a6de92e0f715e119da2035037846223
Will Wallace <wwallace@siteset.co.uk>
2018-11-26 12:31:44 +0000

    New label text for Hero sections in CMS

--- perch/templates/pages/default.php
+++ perch/templates/pages/default.php
@@ -1,7 +1,7 @@
 <?php if (!defined('PERCH_RUNWAY')) include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
 <?php perch_layout("global.head"); ?>
 <?php perch_layout("global.header"); ?>
-<h1>yoooo</h1>
+
 <div class="container main-container">
 	<main class="main wrapper">
 	    <h1 class="page-heading"><?php perch_content('Main heading'); ?></h1>
