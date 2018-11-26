6cdd2a9c93b00ae150ba30c451eca662c59d240b
Will Wallace <wwallace@siteset.co.uk>
2018-11-26 12:14:10 +0000

    Switch 404 from html to php

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php if (!defined('PERCH_RUNWAY')) include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
+<?php perch_layout("global.head"); ?>
+<?php perch_layout("global.header"); ?>
+<div class="container main-container">
+    <main class="main wrapper">
+        <h1>Page Not Found</h1>
+        <p>Sorry, but the page you were trying to view does not exist.</p>
+    </main>
+</div>
+<?php perch_content('Call to action'); ?>
+<?php perch_layout('global.footer'); ?>
+</body>
+</html>
