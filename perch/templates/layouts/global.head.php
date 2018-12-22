d8544cc20d32cf124f3c69762f30aa0ad5efa1aa
Will Wallace <will@willwallace.info>
2018-12-22 17:28:06 +0000

    Edited files from production server. Ugh

--- perch/templates/layouts/global.head.php
+++ perch/templates/layouts/global.head.php
@@ -6,18 +6,27 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title><?php (perch_layout_has("title")?perch_layout_var("title"):perch_pages_title()); ?> - Calibro Transport, Highway &amp; Infrastructure Consultants</title>
-    <meta name="description" content='<?php perch_page_attributes("description"); ?>'>
+    <title><?php (perch_layout_has("title")?perch_layout_var("title"):perch_pages_title()); ?> - Calibro Consultants</title>
+    <?php perch_page_attributes("description"); ?>
+    <link rel="canonical" href="<?php perch_page_url();?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@CalibroConsult">
+    <meta property="og:type" content="website">
+    <meta property="og:title" name="twitter:title" content="<?php (perch_layout_has("title")?perch_layout_var("title"):perch_pages_title());?>">
+    <meta property="og:url" name="twitter:domain" content="<?php perch_page_url();?>">
+    <meta property="og:image" name="twitter:image:src" content="/img/calibro-logo-transparent@2x.png">
+    <meta property="og:site_name" content="Calibro Consultants">
+    <meta property="og:description" name="twitter:description" content="<?php perch_page_attribute("description");?>">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
     <link rel="manifest" href="/manifest.json">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#a1bf35">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg">
     <meta name="theme-color" content="#afcb4a">
     <link rel="stylesheet" href="/css/calibro.min.css">
     <!--[if lt IE 9]><script src="{{'/js/html5shiv.js'}}"></script><![endif]-->
-    <script src="/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <script src="/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js" async></script>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,700" rel="stylesheet">
 </head>
 <body>
